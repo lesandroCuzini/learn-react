622603bbba9d6d4c2d260c080324b2e360c03508
Lesandro de Assis Cuzini <lesandro.assis@gmail.com>
2018-09-20 00:19:05 -0300

    config

--- qa-app/src/config.js
+++ qa-app/src/config.js
@@ -8,20 +8,11 @@ export default {
         URL: "YOUR_API_GATEWAY_URL"
     },
     cognito: {
-<<<<<<< HEAD
-        REGION: "us-east-1",
-        
-        USER_POOL_ID: "us-east-1_ng5jhvU3l",
-        APP_CLIENT_ID: "4mb66rv08eqin3bn46ftmuocnp",
-        IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID",
-        DOMAIN: "https://sisauxadmccb.auth.us-east-1.amazoncognito.com"
-=======
         REGION: "YOUR_COGNITO_REGION",
         
         USER_POOL_ID: "YOUR_USER_POOL_ID",
         APP_CLIENT_ID: "YOUR_APP_CLIENT_ID",
         IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID",
         DOMAIN: "YOUR_DOMAIN_COGNITO"
->>>>>>> 6709e44ee7afb6bc6663609a8c1e30367a9e67e0
     }
 };
