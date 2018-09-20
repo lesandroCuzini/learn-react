79eaaafbbcfd90caba5b740005d53eb271b63916
Lesandro de Assis Cuzini <lesandro.assis@gmail.com>
2018-09-20 00:08:17 -0300

    Add dependencies aws-amplify, react-router-dom, react-router-bootstrap in packages.json

--- qa-app/src/App.js
+++ qa-app/src/App.js
@@ -1,18 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import Routes from './Routes';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Auth } from "aws-amplify";
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+      };
+  }
+  async componentDidMount() {
+
+    try {
+      if (await Auth.currentSession()) {
+      this.userHasAuthenticated(true);
+      }
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+      alert(e);
+      }
+    }
+    
+    this.setState({ isAuthenticating: false });
+  }
+  handleLogout = async event => {
+    await Auth.signOut();
+    
+    this.userHasAuthenticated(false);
+  }
+  changePassword = event => {
+    Auth.currentAuthenticatedUser()
+    .then(user => {
+        return Auth.changePassword(user, 'abc123456', 'abcd12345');
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      !this.state.isAuthenticating &&
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+          <Nav pullRight>
+            {this.state.isAuthenticated ? <NavItem onClick={this.changePassword}>Logout</NavItem> : <Fragment>
+                <LinkContainer to="/signup">
+                <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
+            </Fragment>
+            }
+          </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
       </div>
     );
   }
---
+++ qa-app/src/config.js
@@ -0,0 +1,18 @@
+export default {
+    s3: {
+        REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+        BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+        REGION: "YOUR_API_GATEWAY_REGION",
+        URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        
+        USER_POOL_ID: "us-east-1_ng5jhvU3l",
+        APP_CLIENT_ID: "4mb66rv08eqin3bn46ftmuocnp",
+        IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID",
+        DOMAIN: "https://sisauxadmccb.auth.us-east-1.amazoncognito.com"
+    }
+};
